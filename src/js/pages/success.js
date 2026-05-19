f375d5b1cb7234663daab709dd5e70cbad38e57a
Tobias <tobias.hanevik@outlook.com>
2026-05-19 15:16:18 +0200

    Success page guard
    - Added guard check for isLoggedIn and if cart is not empty

--- src/js/pages/success.js
+++ src/js/pages/success.js
@@ -1,40 +1,55 @@
-import { successContainer, loadingIndicator } from '../constants.js';
+import { successContainer, loadingIndicator, footer } from '../constants.js';
+
+import { getCurrentUser, getSessionToken, isLoggedIn, displayToast } from '../utils.js';
 
 function displaySuccess() {
 
-    loadingIndicator.style.display = 'none';
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const loginCheck = isLoggedIn(getCurrentUser(), getSessionToken());
 
-    const titleContainer = document.createElement('div');
-    titleContainer.classList.add('success-title-container');
-    successContainer.appendChild(titleContainer);
+    if (!loginCheck || cart.length !== 0) {
+        displayToast('Error!', 'You cannot view this page before logging in and completing checkout', 'error');
+            loadingIndicator.style.display = 'none';
+            successContainer.style.display = 'none';
+            setTimeout(() => {
+                navigation.navigate('index.html');
+            }, 2000);
 
-    const h1 = document.createElement('h1');
-    h1.textContent = 'SUCCESS!';
-    titleContainer.appendChild(h1);
+    } else {
+        loadingIndicator.style.display = 'none';
 
-    const successContentContainer = document.createElement('div');
-    successContentContainer.classList.add('succ-content-container');
-    successContainer.appendChild(successContentContainer);
+        const titleContainer = document.createElement('div');
+        titleContainer.classList.add('success-title-container');
+        successContainer.appendChild(titleContainer);
 
-    const span = document.createElement('span');
-    span.classList.add('success-span1');
-    span.textContent = 'Your order has been recieved!';
-    successContentContainer.appendChild(span);
+        const h1 = document.createElement('h1');
+        h1.textContent = 'SUCCESS!';
+        titleContainer.appendChild(h1);
 
-    const span2 = document.createElement('span');
-    span2.classList.add('success-span2');
-    span2.textContent = 'While you wait, you can check out our other products below';
-    successContentContainer.appendChild(span2);
+        const successContentContainer = document.createElement('div');
+        successContentContainer.classList.add('succ-content-container');
+        successContainer.appendChild(successContentContainer);
 
-    const seeAllBtn = document.createElement('button');
-    seeAllBtn.classList.add('see-all-btn');
-    seeAllBtn.ariaLabel = 'Click to see all products';
-    seeAllBtn.textContent = 'SEE ALL PRODUCTS';
-    successContentContainer.appendChild(seeAllBtn);
+        const span = document.createElement('span');
+        span.classList.add('success-span1');
+        span.textContent = 'Your order has been recieved!';
+        successContentContainer.appendChild(span);
 
-    seeAllBtn.addEventListener('click', () => {
-        navigation.navigate('index.html');
-    });
+        const span2 = document.createElement('span');
+        span2.classList.add('success-span2');
+        span2.textContent = 'While you wait, you can check out our other products below';
+        successContentContainer.appendChild(span2);
+
+        const seeAllBtn = document.createElement('button');
+        seeAllBtn.classList.add('see-all-btn');
+        seeAllBtn.ariaLabel = 'Click to see all products';
+        seeAllBtn.textContent = 'SEE ALL PRODUCTS';
+        successContentContainer.appendChild(seeAllBtn);
+
+        seeAllBtn.addEventListener('click', () => {
+            navigation.navigate('index.html');
+        });
+    }
 }
 
 displaySuccess();
